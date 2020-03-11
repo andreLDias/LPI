c4737bb7ecd1de852556fbeb2a18109eba153981
andre-custodio <andre.custodio@redealumni.com>
2020-03-11 19:17:18 -0300

    update

--- exercicios_apostil_FJ11/06 Exercise_six/Exercise_six.java
+++ exercicios_apostil_FJ11/06 Exercise_six/Exercise_six.java
@@ -5,12 +5,11 @@ public class Exercise_six {
         return (n < 2) ? n : Fibonacci(n - 1) + Fibonacci(n - 2);
     }
   
-  
     public static void main(String[] args) {
      
      // teste do programa. Imprime os 30 primeiros termos
-        for (int i = 0; i <= 20; i++) {
-            System.out.println("Termo (" + i + "): " + Fibonacci(i) + "\t");
+        for (int i = 0; i <= 30; i++) {
+            System.out.println("Termo (" + i + "): " + Fibonacci(i));
         }
     }
 }
